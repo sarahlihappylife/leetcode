953b5319dc984e43974928d6ca919c333ca0a07a
kimiwukimi <kimiwukimi@users.noreply.github.com>
2018-12-19 17:46:39 -0500

    Update 853.py

--- 853.py
+++ 853.py
@@ -7,15 +7,19 @@ class Solution(object):
         :rtype: int
         """
         
-        d = {}
-        for i in xrange(len(position)):
-            d[position[i]] = speed[i]
+        d = {pos: spd for pos, spd in zip (postion, speed)}
+#         for i in xrange(len(position)):
+#             d[position[i]] = speed[i]
             
-        res = 0
+#         res = 0
+        cnt = 0
         leadtime = 0
-        for pos, speed in sorted(d.items(), reverse = True):
+#         for pos, speed in sorted(d.items(), reverse = True):
+        for pos, speed in sorted(d.iteritems(), reverse = True):
             curtime = float(target - pos) / speed
             if curtime > leadtime:
                 leadtime = curtime
-                res += 1
-        return res
+#                 res += 1
+                cnt += 1
+#         return res
+        return cnt
