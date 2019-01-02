04bb970cfd9421515ff0aec399c5d4a9eaf28a96
sarah <sarahlihappylife@gmail.com>
2019-01-01 22:17:53 -0500

    152

--- 152.py
+++ 152.py
@@ -5,13 +5,41 @@ class Solution(object):
         :rtype: int
         """
         
+        # curMax = curMin = res = nums[0]
+        
+        # for i in xrange(1, len(nums)):
+        #     if nums[i] < 0:
+        #         curMax, curMin = curMin, curMax
+        #     curMax = max(curMax * nums[i], nums[i])
+        #     curMin = min(curMin * nums[i], nums[i])
+        #     res = max(curMax, res)
+            
+        # return res
+
+
         curMax = curMin = res = nums[0]
+        maxIdx = minIdx = 0
+        resIdx = (0, 0)
         
         for i in xrange(1, len(nums)):
             if nums[i] < 0:
                 curMax, curMin = curMin, curMax
-            curMax = max(curMax * nums[i], nums[i])
-            curMin = min(curMin * nums[i], nums[i])
-            res = max(curMax, res)
+                maxIdx, minIdx = minIdx, maxIdx
+
+            if nums[i] > curMax * nums[i]:
+                maxIdx = i
+                curMax = nums[i]
+            else:
+                curMax = curMax * nums[i]
+
+            if nums[i] < curMin * nums[i]:
+                minIdx = i
+                curMin = nums[i]
+            else:
+                curMin = curMin * nums[i]
+
+            if curMax > res:
+                resIdx = (maxIdx, i)
+                res = curMax
             
-        return res
+        return res, nums[resIdx[0]: resIdx[1] + 1]
